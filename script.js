d5a8ae82d5321c10063107a5eb14cd98e019e61c
Aramayis <akageorgis@gmail.com>
2020-06-23 21:47:03 -0700

    Mobile Fixes, Spotify Playlist

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function myFunction() {
   }
 }
 
-
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
